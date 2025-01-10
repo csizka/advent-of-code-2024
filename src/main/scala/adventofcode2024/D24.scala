604768adf629eb4775abb96ce6f813943688b60d
csizka <csizmadia.kata@gmail.com>
2025-01-10 16:48:59 +0100

    refine d24

--- src/main/scala/adventofcode2024/D24.scala
+++ src/main/scala/adventofcode2024/D24.scala
@@ -2,13 +2,11 @@ package adventofcode2024
 import scala.annotation.tailrec
 import scala.collection.immutable.Queue
 import scala.io.Source
-import guru.nidi.graphviz.attribute.Attributes.attr
-import guru.nidi.graphviz.attribute.{Color, Font, Rank, SimpleLabel, Style}
+import guru.nidi.graphviz.attribute.{Font, Rank}
 import guru.nidi.graphviz.engine.{Format, Graphviz}
 import guru.nidi.graphviz.model.{Node, Factory as gviz}
 import guru.nidi.graphviz.attribute.Rank.RankDir.*
 import guru.nidi.graphviz.model.Factory.{graph, node}
-import guru.nidi.graphviz.model.Link.to
 
 import java.io.File
 import scala.util.Random
@@ -46,8 +44,21 @@ object D24 {
     (gates, startVals)
   }
 
-  def orderToEval(gates: Map[String, InGate], inputVars: Set[String], outPuts: Map[String, Set[String]]): Vector[Gate] = {
+  def reverseDeps(gates: Map[String, InGate]): Map[String, Set[String]] = {
+    gates.foldLeft(Map[String, Set[String]]()) { case (curOuts, out -> InGate(fst, snd, op)) =>
+      curOuts.updatedWith(fst){
+        case Some(set) => Some(set + out)
+        case None => Some(Set(out))
+      }.updatedWith(snd){
+        case Some(set) => Some(set + out)
+        case None => Some(Set(out))
+      }
+    }
+  }
+
+  def orderToEval(gates: Map[String, InGate], inputVars: Set[String]): Vector[Gate] = {
     val startQueue = Queue[String]().enqueueAll(inputVars)
+    val outPuts = reverseDeps(gates)
     @tailrec
     def loop(curQueue: Queue[String], ordered: Vector[Gate], explored: Set[String]): Vector[Gate] = curQueue match {
       case curExplored +: rest if outPuts.contains(curExplored) =>
@@ -78,13 +89,8 @@ object D24 {
     res
   }
 
-  def getDepsAndIngates(gates: Set[Gate]): (Map[String, InGate], Map[String, Set[String]]) = {
-    val (dependencies, outputs) =
-      gates.foldLeft(Map[String, InGate](), Map[String, Set[String]]()) { case ((curDeps, curOuts), Gate(fst, snd, out, op)) =>
-        (curDeps + (out -> InGate(fst, snd, op)),
-          curOuts + (fst -> (curOuts.getOrElse(fst, Set()) + out)) + (snd -> (curOuts.getOrElse(snd, Set()) + out)))
-      }
-    (dependencies, outputs)
+  def getDeps(gates: Set[Gate]): Map[String, InGate] = {
+    gates.map{ case Gate(fst, snd, out, op) => out -> InGate(fst, snd, op)}.toMap
   }
 
   def calcFinalVals(order: Vector[Gate], vals: Map[String, Int]): Map[String, Int] = {
@@ -100,8 +106,8 @@ object D24 {
   }
 
   def d24T1(gates: Set[Gate], vals: Map[String, Int]): Long = {
-    val (dependencies, outputs) = getDepsAndIngates(gates)
-    val order = orderToEval(dependencies, vals.keySet, outputs)
+    val dependencies = getDeps(gates)
+    val order = orderToEval(dependencies, vals.keySet)
     val finalVals = calcFinalVals(order, vals)
     val res = calcDecimalForVars(finalVals, 'z')
     res
@@ -132,52 +138,22 @@ object D24 {
     deps: Map[String, InGate],
     fstToSwap: String,
     sndToSwap: String,
-    inputVars: Set[String],
-    outputs: Map[String, Set[String]]
-  ): (Map[String, InGate], Map[String, Set[String]], Vector[Gate]) = {
+    inputVars: Set[String]
+  ): (Map[String, InGate], Vector[Gate]) = {
     val fstDeps = deps(fstToSwap)
     val sndDeps = deps(sndToSwap)
     val newDeps = deps + (fstToSwap -> sndDeps) + (sndToSwap -> fstDeps)
-    val newOutputs =
-      if (fstDeps.lhsIn != sndDeps.lhsIn
-        && fstDeps.lhsIn != sndDeps.rhsIn
-        && fstDeps.rhsIn != sndDeps.lhsIn
-        && fstDeps.rhsIn != sndDeps.rhsIn){
-        outputs +
-          (fstDeps.lhsIn -> (outputs(fstDeps.lhsIn) - fstToSwap + sndToSwap)) +
-          (fstDeps.rhsIn -> (outputs(fstDeps.rhsIn) - fstToSwap + sndToSwap)) +
-          (sndDeps.lhsIn -> (outputs(sndDeps.lhsIn) - sndToSwap + fstToSwap)) +
-          (sndDeps.rhsIn -> (outputs(sndDeps.rhsIn) - sndToSwap + fstToSwap))
-      }
-      else if (fstDeps.lhsIn == sndDeps.lhsIn && fstDeps.rhsIn != sndDeps.rhsIn)
-        outputs +
-          (fstDeps.rhsIn -> (outputs(fstDeps.rhsIn) - fstToSwap + sndToSwap)) +
-          (sndDeps.rhsIn -> (outputs(sndDeps.rhsIn) - sndToSwap + fstToSwap))
-      else if (fstDeps.rhsIn == sndDeps.lhsIn && fstDeps.lhsIn != sndDeps.rhsIn)
-        outputs +
-          (fstDeps.lhsIn -> (outputs(fstDeps.lhsIn) - fstToSwap + sndToSwap)) +
-          (sndDeps.rhsIn -> (outputs(sndDeps.rhsIn) - sndToSwap + fstToSwap))
-      else if (fstDeps.lhsIn == sndDeps.rhsIn && fstDeps.rhsIn != sndDeps.lhsIn)
-        outputs +
-          (fstDeps.rhsIn -> (outputs(fstDeps.rhsIn) - fstToSwap + sndToSwap)) +
-          (sndDeps.lhsIn -> (outputs(sndDeps.lhsIn) - sndToSwap + fstToSwap))
-      else if (fstDeps.rhsIn == sndDeps.rhsIn && fstDeps.lhsIn != sndDeps.lhsIn)
-        outputs +
-          (fstDeps.lhsIn -> (outputs(fstDeps.lhsIn) - fstToSwap + sndToSwap)) +
-          (sndDeps.lhsIn -> (outputs(sndDeps.lhsIn) - sndToSwap + fstToSwap))
-      else outputs
-    val newOrder = orderToEval(newDeps, inputVars, newOutputs)
-    (newDeps, newOutputs, newOrder)
+    val newOrder = orderToEval(newDeps, inputVars)
+    (newDeps, newOrder)
   }
 
   def d24T2(
     vals: Map[String, Int],
     dependencies: Map[String, InGate],
-    outputs: Map[String, Set[String]],
     swapped: Vector[String]
   ): Vector[String] = {
     val expRes = getExpectedRes(vals)
-    val order = orderToEval(dependencies, vals.keySet, outputs)
+    val order = orderToEval(dependencies, vals.keySet)
     val finalVals = calcFinalVals(order, vals)
     val actualRes = calcDecimalForVars(finalVals, 'z')
     if (actualRes == expRes)
@@ -239,27 +215,27 @@ object D24 {
     toSwap: Vector[String],
     deps:  Map[String, InGate],
     startVals:  Set[String],
-    order: Vector[Gate],
-    outputs:  Map[String, Set[String]]
-  ): (Map[String, InGate], Map[String, Set[String]], Vector[Gate]) = toSwap match {
+    order: Vector[Gate]
+  ): (Map[String, InGate], Vector[Gate]) = toSwap match {
     case fst +: snd +: rest =>
-      val (newDeps, newOutputs, newOrder) = swapGates(deps, fst, snd, startVals, outputs)
-      swaps(rest, newDeps, startVals, newOrder, newOutputs)
-    case Vector() => (deps, outputs, order)
+      val (newDeps, newOrder) = swapGates(deps, fst, snd, startVals)
+      swaps(rest, newDeps, startVals, newOrder)
+    case Vector() => (deps, order)
   }
 
   def main(args: Array[String]): Unit = {
     val (gates, startVals) = parseD24("d24.txt")
-    val (dependencies, outputs) = getDepsAndIngates(gates)
-    val ogOrder = orderToEval(dependencies, startVals.keySet, outputs)
+    val dependencies = getDeps(gates)
+    val ogOrder = orderToEval(dependencies, startVals.keySet)
     val toSwap = Vector("z15", "fph", "z21", "gds", "wrk", "jrs", "cqk", "z34")
-    val (newDeps, newOutputs, newOrder) = swaps(toSwap, dependencies, startVals.keySet, Vector[Gate](), outputs)
-//    val d24t1 = d24T1(gates, startVals)
-    val d24t2 = d24T2(startVals, newDeps, newOutputs, toSwap)
-    visualizeGraph(newDeps)
+    val (newDeps, newOrder) = swaps(toSwap, dependencies, startVals.keySet, Vector[Gate]())
+    val d24t1 = d24T1(gates, startVals)
+    val d24t2 = d24T2(startVals, newDeps, toSwap)
     val fstBadGate = findFstBadGate(newDeps, newOrder)
-    println(fstBadGate)
-//    println(d24t1)
-    println(d24t2)
+    visualizeGraph(newDeps)
+    val revDeps = reverseDeps(dependencies)
+
+    assert(d24t1 == 46463754151024L)
+    assert(d24t2 == Vector("cqk", "fph", "gds", "jrs", "wrk", "z15", "z21", "z34"))
   }
 }
